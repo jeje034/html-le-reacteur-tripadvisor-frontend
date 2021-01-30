2080322a5c060f424f58c523be03d6f471444461
Jeje S <54934028+jeje034@users.noreply.github.com>
2021-01-30 08:25:03 +0100

    Amélioration du formulaire suite à la vidéo de correction

--- assets/js/modal-connection.js
+++ assets/js/modal-connection.js
@@ -7,7 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "click",
         async () => {
             var modal = document.getElementById("modal-connection-background");
-            console.log(modal);
+
+            //console.log(modal);
             modal.style.display = "flex";
             modal.style.justifyContent = "center";
             modal.style.alignItems = "center";
@@ -19,34 +20,50 @@ document.addEventListener("DOMContentLoaded", () => {
                 modal.style.display = "none";
             };
 
-            $.querySelector("#contactForm").addEventListener(
-                "submit",
-                async (event) => {
-                    event.preventDefault();
+            const contactForm = $.querySelector("#contactForm");
 
-                    try {
-                        const data = {
-                            firstname: $.querySelector("#firstname").value,
-                            lastname: $.querySelector("#lastname").value,
-                            email: $.querySelector("#email").value,
-                            subject: $.querySelector("#subject").value,
-                            message: $.querySelector("#message").value,
-                        };
+            contactForm.addEventListener("submit", async (event) => {
+                event.preventDefault();
 
-                        //console.log(data);
+                const sendButton = $.querySelector("#sendButton");
+                //console.log(sendButton);
+                sendButton.setAttribute("disabled", "disabled");
+                sendButton.classList.remove("send-button-enabled");
+                sendButton.classList.add("send-button-disabled");
+                const sendMessage = $.querySelector("#divSendMessage");
+                const title = $.querySelector("#modal-connection-title");
+                //console.log(title);
 
-                        let serverUrl = "http://localhost:3000";
-                        serverUrl =
-                            "https://lereacteur-tripadvisor.herokuapp.com";
+                try {
+                    const data = {
+                        firstname: $.querySelector("#firstname").value,
+                        lastname: $.querySelector("#lastname").value,
+                        email: $.querySelector("#email").value,
+                        subject: $.querySelector("#subject").value,
+                        message: $.querySelector("#message").value,
+                    };
 
-                        await axios.post(serverUrl + "/contact", data);
+                    //console.log(data);
 
-                        alert("Données envoyées.");
-                    } catch (error) {
-                        alert("Erreur en envoi des données : " + error.message);
-                    }
+                    let serverUrl = "http://localhost:3000";
+                    serverUrl = "https://lereacteur-tripadvisor.herokuapp.com";
+
+                    await axios.post(serverUrl + "/contact", data);
+
+                    sendButton.removeAttribute("disabled");
+                    sendButton.classList.remove("send-button-disabled");
+                    sendButton.classList.add("send-button-enabled");
+                    contactForm.reset();
+                    sendMessage.classList.remove("send-message-hidden");
+                    // title.classList.add("less-padding-on-connection-title");
+                    // console.log(title.classList);
+                } catch (error) {
+                    alert("Erreur en envoi des données : " + error.message);
+                    sendButton.removeAttribute("disabled");
+                    sendButton.classList.remove("send-button-disabled");
+                    sendButton.classList.add("send-button-enabled");
                 }
-            );
+            });
 
             // When the user clicks on <span> (x), close the modal
             // On ne fait pas ça sur ancienne version de TripAdvisor
